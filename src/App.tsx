0d6b2ff90575f24dd71fb425ac44c1322009c79d
Lukas Lauraitis <lauraitis.lukas@gmail.com>
2020-06-22 17:47:49 +0300

    pirma diena

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
+const App: React.FC = () =>{
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi i'm the React App</h1>
+      <p>This is realy working</p>
+      <Person name='Mali' age="22" />
+      <Person name='Bakii' age="55"> </Person>
+      <Person name='Aladin' age="99" />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Person/Person.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const person = (props:any ) => {
+return <p>I'm a {props.name} and I'm a {props.age} !</p>
+ }
+
+ export default person;
