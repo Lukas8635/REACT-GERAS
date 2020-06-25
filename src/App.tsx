bacfb546c03ac1d2d14c0005ca425e7df29080e2
Lukas Lauraitis <lauraitis.lukas@gmail.com>
2020-06-25 17:29:42 +0300

    Padaryta iki3.18

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
+import { render } from '@testing-library/react';
 
 
-const App: React.FC = () =>{
- 
-  return(
-    <div className="App">
-      <h1>Hi i'm the React App</h1>
-      <p>This is realy working</p>
-      <button>Swich button</button>
-      <Person name="Alibaba" age="50" />
-      <Person name='Bakii' age="55"> My hobbies: Racing. </Person>
-      <Person name='Aladin' age="99" />
-    </div>
-  );
+const App = () => {
   
-};
+  const [personsState, setPersonsState] = useState({
+    persons:[
+          {name:'max', age:28},
+          {name:'manu', age:29},
+          {name:'aladin', age:26},
+          {name:'simba', age:33}
+        ]
+  });
+  
+  const switchName = (changingName:string, newName:string)=>{
+    let newPersonsState = [...personsState.persons];
+    for(let i = 0; i < personsState.persons.length; i++){
+      if(newPersonsState[i].name === changingName){
+        newPersonsState[i].name = newName;
+      }
+      
+    
+  }
+  const  switchNameHandrel = (newName:string) =>{
+        setPersonsState ({
+          persons:[
+            {name: newName, age:28},
+            {name:'manu', age:29},
+            {name:'aladin', age:26},
+            {name:'simba', age:35455},
+            {name:'aladadas', age:451 }
+          ]
+        });
 
-export default App;
+
+        
+  };
+    return(
+      <div className="App">
+          <h1>Hi i'm a React App</h1>
+          <p>This is realy working</p>
+          <button onClick={()=> switchNameHandrel('pasikeite')}>Swich button</button> 
+          {personsState.persons.map(person=>{
+                 return <Person click={switchNameHandrel.bind(person.name, 'new Name' )} name = {person.name}age={person.age } />  
+                })
+              }
+
+          {/* <Person
+          
+              name={personsState.persons[0].name } 
+              age={personsState.persons[0].age } /> 
+          
+          <Person 
+              name={personsState.persons[1].name } 
+              age={personsState.persons[1].age }
+              click = {switchName.bind('max')}>My hobbie: fishing
+          </Person> 
+          <Person  
+         
+              name={personsState.persons[2].name }
+              age={personsState.persons[2].age } />
+           
+          <Person 
+              name={personsState.persons[3].name }
+              age={personsState.persons[3].age } />  */}
+           
+           
+             
+          
+      </div>
+      );
+  };
+}
+export default App; 
+
+
+
+
+  // state = {
+  //   persons:[
+  //     {name:'max', age:28},
+  //     {name:'manu', age:29},
+  //     {name:'aladin', age:26},
+  //     {name:'simba', age:33}
+  //   ]
+  // }
+
+  // switchNameHandrel = ()=>{
+  //   // console.log('Was clicked ');
+  //   this.setState({
+  //     persons:[
+  //       {name:'maximilian', age:28},
+  //       {name:'manu', age:29},
+  //       {name:'aladin', age:26},
+  //       {name:'simba', age:35455},
+  //       {name:'aladadas', age:451 }
+  //     ]
+  //   })}
+
--- src/Person/Person.tsx
+++ src/Person/Person.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
+interface PersonTypes {
+    name: string;
+    age: number;
+    children?:string;
+    click?:()=> void;
+    
+    
+    
+};
 
-
-const person = (Props:any) => {
-return <div>
-            <p>I'm a {Props.name} and I'm a {Props.age} !</p>
-            <p>{Props.children}</p>
+const person = (props:PersonTypes ) => {
+return( 
+        <div>
+            <p onClick={props.click}>I'm a {props.name} and I'm a {props.age} !</p>
+            <p >{props.children}</p>
+            
         </div>
+    )
  };
 
- export default person; 
+ export default person;  
